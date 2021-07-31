7df19e17881a59a1aceef727c810708192160f68
Alan Signetti <alansignetti@gmail.com>
2021-07-31 11:40:55 -0300

    slider working wrong

--- js/main.js
+++ js/main.js
@@ -1,9 +1,43 @@
-// const slider = document.querySelector("#slider__img");
-// const btnLeft = document.querySelector("#btn-left");
-// const btnRight = document.querySelector("#btn-right");
-
-// // function Next(){
-// //     slider__img.style.marginLeft = "-60%"
-// // }
+let slider = document.querySelector(".slider__img");
+const btnLeft = document.querySelector("#btn-left");
+const btnRight = document.querySelector("#btn-right");
+// let marginLeftSlider = slider.style.marginLeft;
 
 
+function Next(){
+    // let marginLeftSlider = slider.style.marginLeft;
+    
+    slider.style.marginLeft = "-60%";
+    slider.style.transition = "all 0.5s";
+    if(slider.style.marginLeft == "0%"){
+        slider.style.marginLeft = "-60%";
+        slider.style.transition = "all 0.5s";
+    }      
+   
+    if(slider.style.marginLeft == "-60%"){
+        slider.style.marginLeft = "-120%";
+        slider.style.transition = "all 0.5s";
+    }
+    
+    
+    setTimeout(function(){
+        slider.style.transition = "none";
+        slider.style.marginLeft = "-60%";
+    },500)
+}
+
+function Previous(){
+    slider.style.marginLeft = "+60%";
+    slider.style.transition = "all 0.5s";
+    
+}
+
+
+btnRight.addEventListener('click',function(){
+    Next();
+    console.log(slider.style.marginLeft)
+});
+
+btnLeft.addEventListener('click',function(){
+    Previous();
+});
