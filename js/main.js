e2c5fecd0303c4cb34e92fc62364bfd07bdcdca8
Alan Signetti <alansignetti@gmail.com>
2021-07-31 10:37:11 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// const slider = document.querySelector("#slider__img");
+// const btnLeft = document.querySelector("#btn-left");
+// const btnRight = document.querySelector("#btn-right");
+
+// // function Next(){
+// //     slider__img.style.marginLeft = "-60%"
+// // }
+
+
