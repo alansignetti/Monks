e31eb24c5be990c13683e9575409d826480f6e21
Alan Signetti <alansignetti@gmail.com>
2021-08-02 00:23:50 -0300

    slider functionality

--- js/main.js
+++ js/main.js
@@ -2,40 +2,74 @@ let slider = document.querySelector(".slider__img");
 const btnLeft = document.querySelector("#btn-left");
 const btnRight = document.querySelector("#btn-right");
 // let marginLeftSlider = slider.style.marginLeft;
-
+let counter = 0;
 
 function Next(){
     // let marginLeftSlider = slider.style.marginLeft;
-    
-    slider.style.marginLeft = "-60%";
-    slider.style.transition = "all 0.5s";
-    if(slider.style.marginLeft == "0%"){
-        slider.style.marginLeft = "-60%";
-        slider.style.transition = "all 0.5s";
+    slider.style.transition = "all 1s";
+    if(counter == 6){ //slider.style.marginLeft == "0%" && 
+        slider.style.marginLeft = "-520%";
+        
+        counter ++;
+    }   
+    if(counter == 5){ //slider.style.marginLeft == "0%" && 
+        slider.style.marginLeft = "-450%";
+        
+        counter ++;
+    }   
+    if(counter == 5){ //slider.style.marginLeft == "0%" && 
+        slider.style.marginLeft = "-360%";
+        // slider.style.transition = "all 1s";
+        counter ++;
+    }   
+    if(counter == 4){ //slider.style.marginLeft == "0%" && 
+        slider.style.marginLeft = "-290%";
+        // slider.style.transition = "all 1s";
+        counter ++;
+    }   
+    if(counter == 3){ //slider.style.marginLeft == "0%" && 
+        slider.style.marginLeft = "-230%";
+        // slider.style.transition = "all 1s";
+        counter ++;
+    }   
+    if(counter == 2){ //slider.style.marginLeft == "0%" && 
+        slider.style.marginLeft = "-170%";
+        // slider.style.transition = "all 1s";
+        counter ++;
+    }   
+    if(counter == 1){ //slider.style.marginLeft == "0%" && 
+        slider.style.marginLeft = "-100%";
+        // slider.style.transition = "all 1s";
+        counter ++;
+    }   
+    if(counter == 0){ //slider.style.marginLeft == "0%" && 
+        slider.style.marginLeft = "-55%";
+        // slider.style.transition = "all 1s";
+        counter ++;
     }      
-   
-    if(slider.style.marginLeft == "-60%"){
-        slider.style.marginLeft = "-120%";
-        slider.style.transition = "all 0.5s";
-    }
+      
     
-    
-    setTimeout(function(){
-        slider.style.transition = "none";
-        slider.style.marginLeft = "-60%";
-    },500)
+    // setTimeout(function(){
+    //     slider.style.transition = "none";
+    //     // if(counter == 0){
+    //     // slider.style.marginLeft = "-60%";
+    //     // }
+    //     // if(counter == 1){
+    //     //     slider.style.marginLeft = "-120%";
+    //     // }
+    // },500)
 }
 
 function Previous(){
-    slider.style.marginLeft = "+60%";
-    slider.style.transition = "all 0.5s";
-    
+    slider.style.marginLeft = "0%";
+    slider.style.transition = "all 1.5s";
+    counter = 0;
 }
 
 
 btnRight.addEventListener('click',function(){
     Next();
-    console.log(slider.style.marginLeft)
+    
 });
 
 btnLeft.addEventListener('click',function(){
